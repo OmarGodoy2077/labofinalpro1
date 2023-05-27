fa3981058a0d494dd1eb33773e19d6228402cd2a
Omar Godoy <Omar Godoy@DESKTOP-PLQLN95>
2023-05-27 12:46:59 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Empleados.cs
@@ -0,0 +1,25 @@
+﻿public class Empleado
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+
+    public int Edad { get; set; }
+
+    public int Sueldo { get; set; }
+    public string Cargo { get; set; }
+
+    public Empleado(int id, string nombre, string apellido, int edad, string cargo, int sueldo)
+    {
+        Id = id;
+        Nombre = nombre;
+        this.Apellido = apellido;
+        Edad = edad;
+        Cargo = cargo;
+        Sueldo = Sueldo;
+    }
+    public override string ToString()
+    {
+        return $"Id: {Id} Nombre: {Nombre} Apellido: {Apellido} Edad: {Edad} Cargo: {Cargo}";
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+  GestorEmpleados empleados = new GestorEmpleados("c:\\tmp\\Empleados.db");
+//empleados.CrearTabla();
+
+Empleado empleado1 = new Empleado(1, "John", "Doe", 30, "Gerente", 2400);
+Empleado empleado2 = new Empleado(2, "John", "Doe", 30, "Gerente", 2400);
+Empleado empleado3 = new Empleado(3, "John", "Doe", 30, "Gerente", 2400);
+Empleado empleado4 = new Empleado(4, "John", "Doe", 30, "Gerente", 2400);
+
+empleados.AgregarEmpleado(empleado1);
+empleados.AgregarEmpleado(empleado2);
+empleados.AgregarEmpleado(empleado3);
+empleados.AgregarEmpleado(empleado4);
+
+empleados.ObtenerEmpleados();
+//sueldo camposA<<<
+
+
+
+empleados.ObtenerEmpleados().ForEach(e => Console.WriteLine($"{ e.Nombre} - {e.Apellido} - {e.Edad} - {e.Cargo} - {e.Sueldo}"));
+
+
